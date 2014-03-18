8d230f3410e97c2482eb633163ab296edb00dd99
tesstryan <tesstryan@gmail.com>
2014-03-18 15:07:49 -0500

    Fixed Heroku Precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ FourBySix::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # for devise, add it to production when ready to deploy.
   config.action_mailer.default_url_options = { :host => 'http://fourbysix.herokuapp.com/' }
@@ -39,7 +39,7 @@ FourBySix::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
